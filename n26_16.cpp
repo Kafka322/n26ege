30a6692281b3a1878c9812cd272fb78bb16bfc37
Sheryaev Sergey <81568974+Kafka322@users.noreply.github.com>
2021-04-24 13:36:05 +0300

    Create n26_16.cpp

---
+++ n26_16.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<fstream>
+
+using namespace std;
+// inf_22_10_20_26.txt
+int main() {
+
+	ifstream fin("inf_22_10_20_26.txt");
+	int N;
+	fin >> N;
+
+	vector<int> data(N);
+	int sum = 0;
+	int let = 0;
+	for (int i = 0; i < N; i++) {
+		int val;
+		fin >> val;
+		if (val <= 50) {
+			sum += val;
+		}
+		else {
+			data[let] = val;
+			let++;
+		}
+	}
+
+	sort(data.begin(), data.end());
+	int length = N - (N - let);
+	
+	int max = 0;
+	
+	for (int i = N - let; i < (length / 2) + N - let; i++) {
+		sum += round(data[i] * 0.75);
+		if (max < data[i]) max = data[i];
+	} 
+	for (int i = (length / 2) + N - let; i < N; i++) {
+		sum += data[i];
+	}
+
+	cout << sum << " " << max;
+}
